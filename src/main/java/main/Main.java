c0e364c680bb109518c22e284d41ae9bb634f139
Anton Moroz <anton.moroz@helsinki.fi>
2017-11-05 15:11:17 +0200

    gradleHarjoitus-hakemiston import poistettu väliaikaisesti Codacyn valituksen vuoksi

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,7 +1,7 @@
 package main;
 
-import ohtu.ohtuvarasto.Varasto;
-
+//Otetaan import väliaikaisesti pois, Codecov valittaa importin turhuudesta, kun main-metodin sisällä ei ole mitään
+//import ohtu.ohtuvarasto.Varasto;
 public class Main {
 
     public static void main(String[] args) {
